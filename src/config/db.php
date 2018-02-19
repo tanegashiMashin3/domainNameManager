6116cafcb5be4d9d39634dd8e4c519abc0ec9d8f
t_maruyama <maruco6280113@gmail.com>
2018-02-19 23:40:43 +0900

    テスト用のDB設定ファイルを更新

--- src/config/db.php
+++ src/config/db.php
@@ -2,9 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=domainNameManager',
     'username' => 'root',
     'password' => '',
+    // 'username' => 'vagrant',
+    // 'password' => 'AdminX',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
